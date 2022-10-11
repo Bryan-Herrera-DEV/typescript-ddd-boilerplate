c9e2f683092a0c2655696af7122acfeb15f875fd
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-11 08:36:58 -0500

    [:construction: work in progress] añade init para la creacion de los contextos

---
+++ src/_lib/Context.ts
@@ -0,0 +1,10 @@
+type ContextOptions = {
+  shutdownTimeout: number;
+  logger: Pick<Console, 'info' | 'error' | 'warn'>;
+};
+
+const defaultOptions: ContextOptions = {
+  shutdownTimeout: 5000,
+  logger: console,
+}
+const makeContext = () => {}
