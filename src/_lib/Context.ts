8dec3b0de3b3d9371b862073eeadbe20c64e42ec
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-11 11:18:19 -0500

    [:sparkles: feature] añade integracion del Application con la creacion dle contexto

--- src/_lib/Context.ts
+++ src/_lib/Context.ts
@@ -1,3 +1,5 @@
+import { makeApp } from '@/_lib/Application';
+
 type ContextOptions = {
   shutdownTimeout: number;
   logger: Pick<Console, 'info' | 'error' | 'warn'>;
@@ -7,8 +9,13 @@ const defaultOptions: ContextOptions = {
   shutdownTimeout: 5000,
   logger: console,
 }
+
 const makeContext = (opts: Partial<ContextOptions> = {}) => {
   const { shutdownTimeout, logger } = { ...defaultOptions, ...opts };
 
   const moduleKey = Symbol();
+
+  const app = makeApp({shutdownTimeout, logger});
+
+  const bootstrap = async (...modules: any) =>{}
 }
