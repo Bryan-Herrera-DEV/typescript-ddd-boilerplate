fb7aea2afc5ea023adaece38e3833b5445928ed8
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-12 08:22:08 -0500

    [:sparkles: feature] añade validacion de modulos inyectados

--- src/_lib/Context.ts
+++ src/_lib/Context.ts
@@ -1,8 +1,13 @@
 import { makeApp, HookFn, Application } from '@/_lib/Application';
 
-type BootFn<T extends Record<string | symbol, any>> = (arg: Context<T>) => Promise<void | HookFn>;
+type BootFn<T extends Record<string | symbol, any>> = (
+  arg: Context<T>
+) => Promise<void | HookFn>;
 
-type Module<T extends Record<string | symbol, any>, F extends BootFn<T> = BootFn<any>> = {
+type Module<
+  T extends Record<string | symbol, any>,
+  F extends BootFn<T> = BootFn<any>
+> = {
   name: string;
   fn: F;
 };
@@ -24,12 +29,21 @@ const defaultOptions: ContextOptions = {
   logger: console,
 };
 
-const makeContext = <T extends Record<string | symbol, any>>(localContext: T, opts: Partial<ContextOptions> = {}) => {
+const makeContext = <T extends Record<string | symbol, any>>(
+  localContext: T,
+  opts: Partial<ContextOptions> = {}
+) => {
   const { shutdownTimeout, logger } = { ...defaultOptions, ...opts };
 
   const moduleKey = Symbol();
 
   const app = makeApp({ shutdownTimeout, logger });
 
-  const bootstrap = async (...modules: any): Promise<void> => {};
+  const bootstrap = async <M extends Module<T>[]>(...modules: M): Promise<void> => {
+    /* Valida que exista modulos antes de continuar*/
+    if(!modules.every((module) => module[moduleKey])){
+      const foreignModules = modules.filter((module) => !module[moduleKey])
+      throw new Error(`Módulo(s) proporcionado(s) para la función bootstrap:  ${foreignModules.join(', ')}`);
+    }
+  };
 };
