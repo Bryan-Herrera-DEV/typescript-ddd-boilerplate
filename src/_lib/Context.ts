e47f0d4cb89d3817aaf72c489e4dbef3ffa54322
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-12 08:31:46 -0500

    [:sparkles: feature] añade result al contecto

--- src/_lib/Context.ts
+++ src/_lib/Context.ts
@@ -45,9 +45,38 @@ const makeContext = <T extends Record<string | symbol, any>>(
       const foreignModules = modules.filter((module) => !module[moduleKey])
       throw new Error(`Módulo(s) proporcionado(s) para la función bootstrap:  ${foreignModules.join(', ')}`);
     }
+    const context: Context<T> = {
+      ...localContext,
+      app,
+      bootstrap,
+    };
 
     const bootOrder = modules.map(({ name, fn }) => async () => {
       logger.info(`Lanzando Modulo ${name}.`);
+
+      const result = await fn(
+        Object.freeze({
+          ...context,
+          app: app.decorateHooks((lifecycle, fn) => async () => {
+            const isArray = Array.isArray(fn);
+
+            logger.info(`Corriendo ${lifecycle.toLocaleLowerCase()} hook${isArray ? 's' : ''} desde el modulo $name`)
+
+            return (Array.isArray(fn) ? fn : [fn]).reduce(
+              (chain, hook) =>
+                chain.then(() =>
+                  hook().catch((err) => {
+                    logger.error(
+                      `Error while performing ${lifecycle.toLowerCase()} hook${isArray ? 's' : ''} from ${name} module.`
+                    );
+                    logger.error(err);
+                  })
+                ),
+              Promise.resolve()
+            );
+          })
+        })
+      );
 
     })
   }
