68cd96788f5e6aa8fb6c884715fb312719fbbd01
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-12 08:12:42 -0500

    [:hammer: refactor] arregla typing del makeContext

--- src/_lib/Context.ts
+++ src/_lib/Context.ts
@@ -22,14 +22,14 @@ type ContextOptions = {
 const defaultOptions: ContextOptions = {
   shutdownTimeout: 5000,
   logger: console,
-}
+};
 
-const makeContext = (opts: Partial<ContextOptions> = {}) => {
+const makeContext = <T extends Record<string | symbol, any>>(localContext: T, opts: Partial<ContextOptions> = {}) => {
   const { shutdownTimeout, logger } = { ...defaultOptions, ...opts };
 
   const moduleKey = Symbol();
 
-  const app = makeApp({shutdownTimeout, logger});
+  const app = makeApp({ shutdownTimeout, logger });
 
-  const bootstrap = async (...modules: any): Promise<void> =>{}
-}
+  const bootstrap = async (...modules: any): Promise<void> => {};
+};
