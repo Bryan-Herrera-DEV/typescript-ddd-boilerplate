73daeb05da183f71ae6ba0e18c953a030200d014
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-12 17:06:15 -0500

    [:sparkles: feature] añade TypeGuard

--- src/_lib/Environment.ts
+++ src/_lib/Environment.ts
@@ -7,3 +7,25 @@ dotenv.config({
   ? `.env.${process.env.NODE_ENV}.local`
   : `.env.${process.env.NODE_ENV}`,
 })
+
+const environments = ['development', 'production', 'test'] as const;
+
+type EnvironmentTypes = typeof environments[number];
+
+const environment = (defaultValue: EnvironmentTypes = 'development'): EnvironmentTypes => {
+  let env: any = process.env.NODE_ENV;
+
+  if (!env) {
+    env = process.env.NODE_ENV = defaultValue;
+  }
+
+  if (!environments.includes(env)) {
+    throw new TypeError(`Valor no válido para la variable NODE_ENV. Los valores aceptados son: ${environments.join(' | ')}.`);
+  }
+
+  return env;
+};
+
+
+
+export { environment };
