e09594cef1bd1e6903a1f9de5f70678ebd0d1144
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-13 11:08:16 -0500

    [:sparkles: feature] añade handler de las peticiones

---
+++ src/_lib/http/handler.ts
@@ -0,0 +1,21 @@
+import { RequestHandler } from 'express';
+import { asFunction } from 'awilix';
+import { AsyncHandler, runAsync } from '@/_lib/http/runAsync';
+
+type ControllerHandler = (dependencies: any) => AsyncHandler;
+
+const handler = (handler: ControllerHandler): RequestHandler => {
+  const resolver = asFunction(handler);
+
+  return (req, res, next) => {
+    if (!('container' in req)) {
+      throw new Error("No se encuentra el contenedor de peticiones. ¿Ha registrado el middleware `requestContainer`?");
+    }
+
+    const injectedHandler = req.container.build(resolver);
+
+    return runAsync(injectedHandler)(req, res, next);
+  };
+};
+
+export { handler };
