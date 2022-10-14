082bf31a5c01ed63d20999ca59249042baca8672
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-14 16:17:28 -0500

    [:sparkles: feature] modifica skeleton del eventConsumer

--- src/_lib/events/EventConsumer.ts
+++ src/_lib/events/EventConsumer.ts
@@ -1,8 +1,16 @@
+import { Event, EventAddress } from '@/_lib/events/Event';
 const makeEventConsumer =
-  (subscriberKey) =>
-  (address, fn, opts = {}) =>
-  (): void => {};
-() => {};
+<S extends string = 'subscriber'>(subscriberKey: S = 'subscriber' as S) =>
+<E extends Event<any>, D extends Record<string, any> | void = void, OPTS = any>(
+  address: EventAddress<E['eventType'], E['topic']>,
+  fn: (deps: D) => (event: E) => Promise<void>,
+  opts: Partial<OPTS> = {}
+) =>
+(deps: D & { [key in S]: any }): void => {
+  const { [subscriberKey]: subscriber } = deps;
+
+  subscriber.add(address, fn(deps), opts);
+};
 
 const eventConsumer = makeEventConsumer();
 
