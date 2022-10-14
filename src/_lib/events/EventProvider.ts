cdac39c5e823fcad1d6202d473ec890c994a9359
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-14 17:29:19 -0500

    [:sparkles: feature] añade types del publisher

--- src/_lib/events/EventProvider.ts
+++ src/_lib/events/EventProvider.ts
@@ -1,3 +1,13 @@
+import { Event } from '@/_lib/events/Event';
+import { ApplicationService } from '@/_lib/DDD';
+
+type Enqueue = <E extends Event<any>>(event: E) => void;
+type EventStore = {
+  enqueue: Enqueue;
+  getEvents: () => ReadonlyArray<Event<any>>;
+};
+
+
 const makeEventProvider = () => {}
 
 const makeEventStore = () => {}
