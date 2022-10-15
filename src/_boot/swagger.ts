63e77e3466d84ff7cade5b22c9b260b31f63c618
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-10-15 17:46:06 -0500

    [:sparkles:/:hammer: features/refactor]

--- src/_boot/swagger.ts
+++ src/_boot/swagger.ts
@@ -14,21 +14,38 @@ type SwaggerConfig = {
 
 const swagger = makeModule('swagger', async ({ container: { build }, config: { http, swagger } }) => {
   const options = {
-    swaggerDefinition: {
+    definition: {
       info: {
         title: swagger.title,
         version: swagger.version,
+        description:
+        "Este es un simple ejemplo de API con arquitctura Hexagonal",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html"
+        },
+        contact: {
+          name: "Bryan-Herrrera-DEV",
+          url: "https://bryan-herrera.netlify.app",
+          email: "bryherrera55@gmail.com",
+        },
       },
+      servers: [
+        {
+          url: "http://localhost:5000/api",
+        },
+      ],
       basePath: swagger.basePath,
     },
     apis: [resolve(__dirname, '../**/interface/http/**/*.yaml'), resolve(__dirname, '../**/interface/http/**/*.ts')],
+
   };
 
   // Initialize swagger-jsdoc -> returns validated swagger spec in json format
   const swaggerSpec = swaggerJSDoc(options);
 
   build(({ server }) => {
-    server.use(swagger.docEndpoint, swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
+    server.use(swagger.docEndpoint, swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true, customSiteTitle: 'DDD boilerplate'}));
   });
 });
 
